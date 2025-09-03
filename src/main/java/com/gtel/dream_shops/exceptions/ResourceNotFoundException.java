4d49537c459414c611fe8f9de6d14b2bc965bd6d
trungsliver <buiductrung.cva@gmail.com>
2025-09-03 15:05:42 +0700

    category service

---
+++ src/main/java/com/gtel/dream_shops/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gtel.dream_shops.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public  ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/gtel/dream_shops/service/category/ICategoryService.java
@@ -0,0 +1,14 @@
+package com.gtel.dream_shops.service.category;
+
+import com.gtel.dream_shops.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    void deleteCategory(Long id);
+}
