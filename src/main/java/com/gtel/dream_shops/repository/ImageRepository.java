03490fbb98a68d07ee53c468783eb88fd574a6ef
trungsliver <buiductrung.cva@gmail.com>
2025-09-03 15:37:41 +0700

    image service

---
+++ src/main/java/com/gtel/dream_shops/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.gtel.dream_shops.repository;
+
+import com.gtel.dream_shops.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image,Long> {
+}
---
+++ src/main/java/com/gtel/dream_shops/service/image/IImageService.java
@@ -0,0 +1,14 @@
+package com.gtel.dream_shops.service.image;
+
+import com.gtel.dream_shops.dto.ImageDto;
+import com.gtel.dream_shops.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IImageService {
+    Image getImageById(Long id);
+    void deleteImageById(Long id);
+    List<ImageDto> saveImages(List<MultipartFile> files, Long productId);
+    void updateImage(MultipartFile file, Long imageId);
+}
