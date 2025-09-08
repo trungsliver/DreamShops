43cb7746685cfd3cf15d4695f3ec5504ebdfde13
trungsliver <buiductrung.cva@gmail.com>
2025-09-08 17:11:17 +0700

    shop user detail service

--- src/main/java/com/gtel/dream_shops/sercurity/user/ShopUserDetailsService.java
+++ src/main/java/com/gtel/dream_shops/sercurity/user/ShopUserDetailsService.java
@@ -1,13 +1,23 @@
 package com.gtel.dream_shops.sercurity.user;
 
+import com.gtel.dream_shops.model.User;
+import com.gtel.dream_shops.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ShopUserDetailsService implements UserDetailsService {
-
+    private final UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+        return ShopUserDetails.buildUserDetails(user);
     }
 }
