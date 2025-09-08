0552f284ab5e7e346132552018b0035b9a41d3a3
trungsliver <buiductrung.cva@gmail.com>
2025-09-08 16:54:37 +0700

    start spring sercurity

---
+++ src/main/java/com/gtel/dream_shops/sercurity/user/ShopUserDetailsService.java
@@ -0,0 +1,13 @@
+package com.gtel.dream_shops.sercurity.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class ShopUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
