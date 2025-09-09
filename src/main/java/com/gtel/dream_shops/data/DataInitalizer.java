8c5202636d693a1f2a12aff81ab675cee391cc40
trungsliver <buiductrung.cva@gmail.com>
2025-09-09 14:00:11 +0700

    set role for default user & admin

--- src/main/java/com/gtel/dream_shops/data/DataInitalizer.java
+++ src/main/java/com/gtel/dream_shops/data/DataInitalizer.java
@@ -1,12 +1,14 @@
 package com.gtel.dream_shops.data;
 
 //import com.gtel.dream_shops.model.Role;
+import com.gtel.dream_shops.model.Role;
 import com.gtel.dream_shops.model.User;
 import com.gtel.dream_shops.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
@@ -16,20 +18,20 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class DataInitalizer implements ApplicationListener<ApplicationReadyEvent> {
     private final UserRepository userRepository;
-//    private final RoleRepository roleRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-//        Set<String> defaultRoles =  Set.of("ROLE_ADMIN", "ROLE_USER");
+        Set<String> defaultRoles =  Set.of("ROLE_ADMIN", "ROLE_USER");
         createDefaultUserIfNotExits();
-//        createDefaultRoleIfNotExits(defaultRoles);
-//        createDefaultAdminIfNotExits();
+        createDefaultRoleIfNotExits(defaultRoles);
+        createDefaultAdminIfNotExits();
     }
 
 
     private void createDefaultUserIfNotExits(){
-//        Role userRole = roleRepository.findByName("ROLE_USER").get();
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
         for (int i = 1; i<=5; i++){
             String defaultEmail = "user"+i+"@email.com";
             if (userRepository.existsByEmail(defaultEmail)){
@@ -39,38 +41,36 @@ public class DataInitalizer implements ApplicationListener<ApplicationReadyEvent
             user.setFirstName("The User");
             user.setLastName("User" + i);
             user.setEmail(defaultEmail);
-            user.setPassword("123456");
-//            user.setPassword(passwordEncoder.encode("123456"));
-//            user.setRoles(Set.of(userRole));
+//            user.setPassword("123456");
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(userRole));
             userRepository.save(user);
             System.out.println("Default vet user " + i + " created successfully.");
         }
     }
 
-
-//
-//    private void createDefaultAdminIfNotExits(){
-//        Role adminRole = roleRepository.findByName("ROLE_ADMIN").get();
-//        for (int i = 1; i<=2; i++){
-//            String defaultEmail = "admin"+i+"@email.com";
-//            if (userRepository.existsByEmail(defaultEmail)){
-//                continue;
-//            }
-//            User user = new User();
-//            user.setFirstName("Admin");
-//            user.setLastName("Admin" + i);
-//            user.setEmail(defaultEmail);
+    private void createDefaultAdminIfNotExits(){
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN").get();
+        for (int i = 1; i<=2; i++){
+            String defaultEmail = "admin"+i+"@email.com";
+            if (userRepository.existsByEmail(defaultEmail)){
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("Admin");
+            user.setLastName("Admin" + i);
+            user.setEmail(defaultEmail);
 //            user.setPassword("123456");
-////            user.setPassword(passwordEncoder.encode("123456"));
-////            user.setRoles(Set.of(adminRole));
-//            userRepository.save(user);
-//            System.out.println("Default admin user " + i + " created successfully.");
-//        }
-//    }
-//    private void createDefaultRoleIfNotExits(Set<String> roles){
-//        roles.stream()
-//                .filter(role -> roleRepository.findByName(role).isEmpty())
-//                .map(Role:: new).forEach(roleRepository::save);
-//
-//    }
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(adminRole));
+            userRepository.save(user);
+            System.out.println("Default admin user " + i + " created successfully.");
+        }
+    }
+    private void createDefaultRoleIfNotExits(Set<String> roles){
+        roles.stream()
+                .filter(role -> roleRepository.findByName(role).isEmpty())
+                .map(Role:: new).forEach(roleRepository::save);
+
+    }
 }
